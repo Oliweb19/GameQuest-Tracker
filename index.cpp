d6382551a582f3742d608ea9d347a6b4af4d7b72
Oliweb19 <oaguillen.24@est.ucab.edu.ve>
2025-05-29 21:47:28 -0400

    Ya se trabaja con el menu

--- index.cpp
+++ index.cpp
@@ -1,15 +1,28 @@
 #include "gameguest.h"
 
-
-
 main(){
     Jugador *perfiles = nullptr;
-    int i = 1;
+    int op = 1;
 
-    while(i <= 3){
-        AgregarJugador(&perfiles);
-        ImprimirJugadores(perfiles);
+    while(op != 0){
+        MenuLogueo();
+        cout<< "Ingrese una opcion: ";
+        cin>> op;
 
-        i++;
+        switch (op){
+            case 1:
+                /* Parte de logueo */
+                break;
+            case 2:
+                AgregarJugador(&perfiles);
+                break;
+            
+            default:
+                cout<< "Opcion Invalida, ingrese una opcion valida"<<endl;
+                break;
+        }
     }
+
+
+    
 }
